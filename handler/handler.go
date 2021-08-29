997e454728fe7a0e6e8810cc8e399627d294cebd
Rodolfo Martínez Vega <rodomar@outlook.com>
2021-08-29 14:41:01 -0500

    - Improve request and response structs - Fix HTTP response status to HTTP 200 OK

--- handler/handler.go
+++ handler/handler.go
@@ -7,14 +7,23 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/ramrodo/tech-assessment-loan-startup/model"
 	"github.com/ramrodo/tech-assessment-loan-startup/service"
 	log "github.com/sirupsen/logrus"
 )
 
+type CreditAssignmentRequest struct {
+	Amount int32 `json:"investment" validate:"required"`
+}
+
+type CreditAssignmentResponse struct {
+	CreditType300 int32 `json:"credit_type_300"`
+	CreditType500 int32 `json:"credit_type_500"`
+	CreditType700 int32 `json:"credit_type_700"`
+}
+
 // CreditAssignment - returns the Investment assigned
 func CreditAssignment(w http.ResponseWriter, r *http.Request) {
-	var investment model.Investment
+	input := &CreditAssignmentRequest{}
 
 	// This is a good way to protect against malicious attacks on your server limiting JSON size
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
@@ -27,7 +36,7 @@ func CreditAssignment(w http.ResponseWriter, r *http.Request) {
 		panic(fmt.Sprintf("Error at closing body: %s", err))
 	}
 
-	if err := json.Unmarshal(body, &investment); err != nil {
+	if err := json.Unmarshal(body, &input); err != nil {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(422) // HTTP 422: unprocessable entity
 		if err := json.NewEncoder(w).Encode(err); err != nil {
@@ -36,7 +45,7 @@ func CreditAssignment(w http.ResponseWriter, r *http.Request) {
 	}
 
 	investmentService := service.NewCreditAssigner()
-	credit_type_300, credit_type_500, credit_type_700, err := investmentService.Assign(investment.Amount)
+	credit_type_300, credit_type_500, credit_type_700, err := investmentService.Assign(input.Amount)
 
 	if err != nil {
 		log.Error(err)
@@ -44,15 +53,15 @@ func CreditAssignment(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(400)
 	}
 
-	creditTypes := model.CreditTypes{
+	res := &CreditAssignmentResponse{
 		CreditType300: credit_type_300,
 		CreditType500: credit_type_500,
 		CreditType700: credit_type_700,
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(creditTypes); err != nil {
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(res); err != nil {
 		panic(fmt.Sprintf("Error at unmarshalling body: %s", err))
 	}
 
