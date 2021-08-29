0457168aff1f62d0c33210bb01c2c3630670f87b
Rodolfo Martínez Vega <rodomar@outlook.com>
2021-08-29 14:41:16 -0500

    Removed unnecessary models for now

--- model/creditTypes.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// CreditTypes - struct
-type CreditTypes struct {
-	CreditType300 int32 `json:"credit_type_300"`
-	CreditType500 int32 `json:"credit_type_500"`
-	CreditType700 int32 `json:"credit_type_700"`
-}
--- model/investment.go
+++
@@ -1,6 +0,0 @@
-package model
-
-// Investment - struct
-type Investment struct {
-	Amount int32 `json:"investment"`
-}
